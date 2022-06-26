2117d4fdfb8e36dbb6b2ee5e7b66a8faaf3dbcc7
pouyasadri <pouyapaghandeh78@gmail.com>
2022-06-26 18:20:07 +0200

    26/06/2022

---
+++ cmd/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookSotreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
